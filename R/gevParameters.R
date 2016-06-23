016dcdd473835126f5daf4b97c05976d78b29185
MartinRoth <marsianus@gmail.com>
2016-06-23 17:18:14 +0200

    Introduced fixed gev parameters

--- R/gevParameters.R
+++ R/gevParameters.R
@@ -1,8 +1,24 @@
-#' Calculate GEV parameters
+#' Calculate GEV Parameters
 #'
-#' @importFrom extRemes fevd
+#' @description Calculate GEV parameters using MLE
+#' @param x numeric vector containing the data
+#'
+#' @importFrom evd fgev
 #' @export
 GetGevParams <- function(x) {
-  tmp <- fevd(x, type = "GEV", method = "Lmoments")
-  tmp$results
+  tmp <- fgev(x, std.err=FALSE, warn.inf = FALSE)
+  as.list(tmp$estimate)
+}
+
+#' Calculate GEV Parameters (Fixed Shape)
+#'
+#' @description Calculate GEV parameters using MLE
+#' @param x numeric vector containing the data
+#' @param shape numeric value
+#'
+#' @importFrom evd fgev
+#' @export
+GetGevParamsFixedShape <- function(x, shape) {
+  tmp <- fgev(x, shape = shape, std.err = FALSE, warn.inf = FALSE)
+  as.list(tmp$estimate)
 }
