511359ae940fb951aef9e7f8498a0c897323d309
MartinRoth <marsianus@gmail.com>
2016-06-28 11:31:55 +0200

    Removed February 29 from estimate plot

--- R/plotting.R
+++ R/plotting.R
@@ -9,8 +9,8 @@ PlotEstimates <- function(x) {
   setnames(oldValues, names(values))
   values <- melt(values, id.vars = "displayDate", variable.name = "parameter")
   oldValues <- melt(oldValues, id.vars = "displayDate", variable.name = "parameter")
-  ggplot(oldValues, aes(x = displayDate, y = value)) + geom_point() +
-    geom_line(col=2, data = values) +
+  ggplot(oldValues[displayDate != "2016-02-29"], aes(x = displayDate, y = value)) + geom_point() +
+    geom_line(col=2, data = values[displayDate != "2016-02-29"]) +
     facet_wrap(~parameter, scales="free_y", nrow=3) +
     scale_x_date(date_labels = "%b %d") +
     xlab("Day of the year")
